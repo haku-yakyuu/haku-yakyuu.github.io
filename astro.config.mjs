5141743ac371ff663b02dd047ca8e627820cd09e
haku-yakyuu <bohan816@gmail.com>
2025-12-24 14:16:56 +0000

    Install and configure Astro Sitemap integration

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,10 +1,8 @@
 import { defineConfig } from 'astro/config';
+import sitemap from '@astrojs/sitemap';
 
 export default defineConfig({
-  // ⚠️ 1. 請填入你的 GitHub Pages 網址 (例如 https://你的帳號.github.io)
+  // 必須填寫正確的網站網址，Sitemap 才能產生正確的連結
   site: 'https://haku-yakyuu.github.io',
-  
-  // ⚠️ 2. 如果你的儲存庫名稱不是 kae-k.github.io，而是像 "my-shop"，請填入 '/my-shop'
-  // 如果是使用者主頁 (user site)，則留空字串 ''
-  base: '',
+  integrations: [sitemap()],
 });
