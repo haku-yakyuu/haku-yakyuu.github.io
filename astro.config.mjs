f9e729d24ed53c574d2f6b9e5ebfe9bfdeeccf44
haku-yakyuu <bohan816@gmail.com>
2025-12-24 16:02:35 +0000

    feat: add google verification and sitemap integration

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,10 +1,14 @@
 import { defineConfig } from 'astro/config';
 
+import sitemap from '@astrojs/sitemap';
+
 export default defineConfig({
   // ⚠️ 1. 請填入你的 GitHub Pages 網址 (例如 https://你的帳號.github.io)
   site: 'https://haku-yakyuu.github.io',
-  
+
   // ⚠️ 2. 如果你的儲存庫名稱不是 kae-k.github.io，而是像 "my-shop"，請填入 '/my-shop'
   // 如果是使用者主頁 (user site)，則留空字串 ''
   base: '',
+
+  integrations: [sitemap()],
 });
